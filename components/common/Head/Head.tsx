bde8bc404f2a96adfaf99303a78c3f3a741120c4
nguyen nha <nguyenphucnha111@gmail.com>
2021-06-30 19:32:15 +0700

    analyst

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react'
 import NextHead from 'next/head'
 import { DefaultSeo } from 'next-seo'
 import config from '@config/seo.json'
-
 const Head: FC = () => {
   return (
     <>
@@ -12,6 +11,8 @@ const Head: FC = () => {
         <meta name="description" content="DEXI shop là nơi bán tất cả các sản phẩm local brand VIỆT NAM với thương hiệu DEXI."/>
         <meta name="google-site-verification" content="Ytgs3S2_9TvLIaoEYrnc4TEXb36YtYtFZtexy7JECVc"/>
         <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LB545D35PB"></script>
+        <script src="/assets/analyst.js"></script>
       </NextHead>
     </>
   )
