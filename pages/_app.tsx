6f1e98f0f542d2918946529b59aa0044660dba6f
nguyen nha <nguyenphucnha111@gmail.com>
2021-07-01 11:22:09 +0700

    thêm google analyst

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@assets/main.css'
 import '@assets/chrome-bug.css'
 import 'keen-slider/keen-slider.min.css'
-
+import { useRouter } from 'next/router';
 import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
@@ -10,11 +10,22 @@ const Noop: FC = ({ children }) => <>{children}</>
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop
+  const router = useRouter();
+  const handleRouteChange = (url: URL) => {
+    window.gtag('config', 'G-LB545D35PB', {
+      page_path: url,
+    });
+  };
 
   useEffect(() => {
     document.body.classList?.remove('loading')
-  }, [])
 
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+
+  }, [router.events]);
   return (
     <>
       <Head />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LB545D35PB"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LB545D35PB', { page_path: window.location.pathname });
+            `,
+          }}
+        />
+        </Head>
         <body className="loading">
           <Main />
           <NextScript />
